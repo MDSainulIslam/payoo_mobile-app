9cdf5d2c0fe088ba36cf47844ca00077d7f9a7eb
lisan <mdsainulislamlisan@gmail.com>
2025-08-23 18:15:14 +0600

    project done

--- home.js
+++ home.js
@@ -1,5 +1,7 @@
 const validPin = 1234;
 
+// add money featuere
+
 document.getElementById("add_money_button").addEventListener("click",function(e){
     e.preventDefault();
     // console.log("button clicked")
@@ -28,3 +30,32 @@ document.getElementById("Available_balance").innerText = totalNewAvailableBalanc
 console.log(totalNewAvailableBalance);
 })
 
+// cash out money feature
+document.getElementById("withdraw_btn").addEventListener("click",function(e){
+    e.preventDefault();
+    const amount = parseInt(document.getElementById("withdraw_amount").value);
+    const availableBalance = parseInt(document.getElementById("Available_balance").innerText);
+
+    // console.log(amount, availableBalance);
+    const totalNewAvailableBalance = availableBalance - amount;
+    console.log(availableBalance, amount, totalNewAvailableBalance);
+
+document.getElementById("Available_balance").innerText = totalNewAvailableBalance;
+
+
+})
+
+
+
+
+
+// toggling feature
+document.getElementById("add_button").addEventListener("click",function(){
+    document.getElementById("cash_out_parent").style.display = "none";
+    document.getElementById("add_money_parent").style.display = "block";
+})
+
+document.getElementById("cash_out_button").addEventListener("click",function(){
+    document.getElementById("add_money_parent").style.display = "none";
+    document.getElementById("cash_out_parent").style.display = "block";
+})
